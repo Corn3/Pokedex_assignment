90a0d6b9a31e69afc2f8de654e848c99865c1ba7
Pontus Rohden <pontus.rohden@se.experis.com>
2021-08-24 08:10:52 +0200

    Added pages and routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,34 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { RouterModule, Routes } from "@angular/router";
+import { CataloguePageComponent } from "./pages/catalogue/catalogue.page.component";
+import { LandingPageComponent } from "./pages/landing/landing.page.component";
+import { TrainerPageComponent } from "./pages/trainer/trainer.page.component";
 
-const routes: Routes = [];
+
+
+
+const routes: Routes = [
+    {
+        path: '',
+        pathMatch: 'full',
+        redirectTo: '/landing'
+    },
+    {
+        path: 'landing',
+        component: LandingPageComponent
+    },
+    {
+        path: 'trainer',
+        component: TrainerPageComponent
+    },
+    {
+        path: 'catalogue',
+        component: CataloguePageComponent
+    }
+]
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LandingPageComponent } from './pages/landing/landing.page.component'
+import { TrainerPageComponent } from './pages/trainer/trainer.page.component';
+import { CataloguePageComponent } from './pages/catalogue/catalogue.page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LandingPageComponent,
+    TrainerPageComponent,
+    CataloguePageComponent
   ],
   imports: [
     BrowserModule,
+    AppRoutingModule,
     AppRoutingModule
   ],
   providers: [],
