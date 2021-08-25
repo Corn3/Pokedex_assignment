873f96e48e74d6dbc72d2c50d227ad02e56e0882
Corn3 <robineliasson93@gmail.com>
2021-08-25 11:38:01 +0200

    Renamed file, and upadted trainer page.

--- src/app/pages/trainer/trainer.page.ts
+++ src/app/pages/trainer/trainer.page.ts
@@ -12,6 +12,7 @@ export class TrainerPage implements OnInit {
     constructor(private router: Router) {}
 
     ngOnInit() {
+        setStorage("trainer", "Test") // Remove later
         if(getStorage("trainer") === "{}") {
             this.router.navigate(["landing"]);
         }
--- src/app/services/pokemon.service.ts
+++ src/app/services/pokemon.service.ts
@@ -17,26 +17,43 @@ export class PokemonService {
     constructor(private readonly http: HttpClient) {
 
     }
-    public fetchPokemons(): void {
-        this.http.get(this.DEFAULT_URL + '?limit=151')
-            .subscribe((data: any) => {
-                this._pokemons = data.results;
-                this.fetchPokemonStats();
-            }, (error: HttpErrorResponse) => {
-                this._error = error.message;
-            })
+    /**
+     * Saves all first gen pokemon to a private array
+     */
+     public fetchPokemons(): void {
+        this.http.get('https://pokeapi.co/api/v2/pokemon/?limit=151')
+        .subscribe((data: any) => {
+            this._pokemons = data.results;
+            this.fetchPokemonStats();
+        }, (error: HttpErrorResponse) => {
+            this._error = error.message;
+        })
     }
+    /**
+     * Loops through the private pokemon array and fetches the id. 
+     * Then adds the id and image to the Pokemon object.
+     */
     public fetchPokemonStats(): void {
         this._pokemons.forEach(element => {
             element.name = this.capitalizeFirstLetter(element.name);
             element.isCaught = false;
             this.http.get(element.url)
                 .subscribe((data: any) => {
-                    element.id = data.id;
-                }, (error: HttpErrorResponse) => {
-                    this._error = error.message;
-                })
+                element.id = data.id;
+                element.image = "../../../assets/gen1-sprites/" + data.id + ".png";
+            }, (error: HttpErrorResponse) => {
+                this._error = error.message;
+            })           
         });
+    }
+    public getPokemon (pokemon: string): any {
+        let ret = null;
+        this._pokemons.forEach((element: Pokemon) => {
+            if(element.name === pokemon) {
+                ret = element;
+            }
+        })
+        return ret;
     }
 
     private capitalizeFirstLetter(name: string): string {
@@ -50,7 +67,7 @@ export class PokemonService {
             for (const pokemonId of pokemons) {
                 this.http.get<Pokemon[]>(this.DEFAULT_URL + pokemonId)
                     .subscribe((data: any) => {
-                        const pokemon: CaughtPokemon = this.getPokemon(data, pokemonId);
+                        const pokemon: CaughtPokemon = this.getCaughtPokemon(data, pokemonId);
                         this.caughtPokemons.push(pokemon)
                     }, (error: HttpErrorResponse) => {
                         this._error = error.message;
@@ -59,7 +76,7 @@ export class PokemonService {
         }
     }
 
-    getPokemon(data: any, id: number) {
+    getCaughtPokemon(data: any, id: number) {
         let name: string = this.capitalizeFirstLetter(data.forms[0].name);
         const animatedAvatar = data.sprites.versions["generation-v"]["black-white"].animated.front_default;
         const types = this.getTypes(data);
